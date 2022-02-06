1f85e79cc283342d18da19167bf83c9401a99586
AditiaGoti <gotiaditia26@gmail.com>
2022-02-06 23:07:24 +0700

    first commit

---
+++ app/includes/charts/chart_reports.php
@@ -0,0 +1,46 @@
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-md-12 grid-margin">
+                <div class="card">
+                    <div class="p-4 border-bottom bg-light">
+                        <h4 class="card-title mb-0">Nominal Transaksi</h4>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="members-chart" height="100"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 grid-margin">
+                <div class="card">
+                    <div class="p-4 border-bottom bg-light">
+                        <h4 class="card-title mb-0">Nominal Transaksi</h4>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="visitors-chart" height="100"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 grid-margin">
+                <div class="card">
+                    <div class="p-4 border-bottom bg-light">
+                        <h4 class="card-title mb-0">Nominal Transaksi</h4>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="transactions-chart" height="100"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- content-wrapper ends -->
+    <!-- partial:../../partials/_footer.html -->
+    <footer class="footer">
+        <div class="container-fluid clearfix">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+        </div>
+    </footer>
+
+
+    <!-- partial -->
+</div>
---
+++ app/includes/tables/table_transaksi.php
@@ -0,0 +1,59 @@
+        <div class="main-panel">
+            <div class="content-wrapper">
+                <div class="row page-title-header">
+                    <div class="col-12">
+                        <div class="page-header">
+                            <h4 class="page-title">Data Transaksi</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Daftar Transaksi</h4>
+                                <br />
+                                <div class="panel-body">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped table-bordered table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th>No. Transaksi</th>
+                                                    <th>Member ID</th>
+                                                    <th>Nama</th>
+                                                    <th>Tipe Paket</th>
+                                                    <th>Nominal</th>
+                                                    <th>FO</th>
+                                                    <th>Waktu</th>
+                                                    <th>Ket</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>Syahrul Husna</td>
+                                                    <td>Jakarta</td>
+                                                    <td>22 Maret 2000</td>
+                                                    <td>Syahrul Husna</td>
+                                                    <td>syahrulhusna@gmail.com</td>
+                                                    <td>Jl. Sapta No. 88</td>
+                                                    <td>27 Maret 2022</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- content-wrapper ends -->
+            <!-- partial:../../partials/_footer.html -->
+            <footer class="footer">
+                <div class="container-fluid clearfix">
+                    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2022. All Rights Reserved</span>
+                </div>
+            </footer>
+            <!-- partial -->
+        </div>
