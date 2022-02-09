87ea413e0e9e43d9e7e6dcf08eb4f23b9f0f3ce9
AditiaGoti <gotiaditia26@gmail.com>
2022-02-09 15:26:47 +0700

    benerin form member

--- app/includes/forms/form_member.php
+++ app/includes/forms/form_member.php
@@ -120,7 +120,7 @@
     <!-- partial:../../partials/_footer.html -->
     <footer class="footer">
         <div class="container-fluid clearfix">
-            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2022. All Rights Reserved</span>
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©️ 2022. All Rights Reserved</span>
         </div>
     </footer>
     <!-- partial -->
