55877a798fd294b9cd66f0b8d1a953f6ead926f4
AditiaGoti <gotiaditia26@gmail.com>
2022-02-10 22:42:35 +0700

    nambahin script search

--- app/includes/tables/table_member.php
+++ app/includes/tables/table_member.php
@@ -228,6 +228,24 @@
     </div>
 
     <script>
+         function searchTable(col) {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById(col);
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table1");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    if(col=='1') td = tr[i].getElementsByTagName("td")[1];
+    else if(col=='3') td = tr[i].getElementsByTagName("td")[3];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }     
+  }
+}
         function sortTable(d, n) {
             var table, rows, col_header, switching, i, x, y, a, b, shouldSwitch, dir = "asc",
                 switchcount = 0;
