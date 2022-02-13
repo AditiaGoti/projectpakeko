7dc88d3d691fc365bf5a823d33025deb73780776
AditiaGoti <gotiaditia26@gmail.com>
2022-02-13 21:35:34 +0700

    benerin logo

--- app/includes/config/navbar.php
+++ app/includes/config/navbar.php
@@ -1,8 +1,9 @@
 <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-        <a class="navbar-brand brand-logo" href="/admin" style="width: 150px; ">
-            <img src="assets/images/logoo.png" alt="logo" /></a>
+    <a class="navbar-brand brand-logo" href="/owner" style="margin-top: -80px; width: 230px;">
+            <img src="assets/images/aderai.png" alt="logo" /></a>
         <a class="navbar-brand brand-logo-mini" href="/admin">
+        <img src="assets/images/logoo.png" alt="logo" /> 
         </a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-center">
