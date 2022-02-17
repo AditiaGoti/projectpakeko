ad6bd2d7519ddb01bf91a5b5560301967541cc2e
alsi <syahrulhusna@gmail.com>
2022-02-18 02:19:49 +0700

    nav side admin

--- app/includes/config/navbar.php
+++ app/includes/config/navbar.php
@@ -1,55 +1,29 @@
-<nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-    <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-        <a class="navbar-brand brand-logo" href="/owner" style="margin-top: -80px; width: 230px;">
-            <img src="assets/images/aderai.png" alt="logo" /></a>
-        <a class="navbar-brand brand-logo-mini" href="/admin">
-            <img src="assets/images/logoo.png" alt="logo" />
-        </a>
-    </div>
-    <div class="navbar-menu-wrapper d-flex align-items-center">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown d-none d-xl-inline-block user-dropdown">
-                <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-                    <img class="img-xs rounded-circle" src="assets/images/faces/face8.jpg" alt="Profile image"> </a>
-                <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
-                    <div class="dropdown-header text-center">
-                        <img class="img-md rounded-circle" src="assets/images/faces/face8.jpg" alt="Profile image">
-                        <p class="mb-1 mt-3 font-weight-semibold"><?php echo $_SESSION['name'] ?></p>
-                        <p class="font-weight-light text-muted mb-0"><?php echo $_SESSION['email'] ?></p>
-                    </div>
-                    <a href="/profile" class="dropdown-item">My Profile <span class="badge badge-pill badge-danger"></span><i class="dropdown-item-icon ti-dashboard"></i></a>
-                    <a href="/changepass" class="dropdown-item">Change Password <span class="badge badge-pill badge-danger"></span><i class="dropdown-item-icon ti-dashboard"></i></a>
-                    <a onclick="logout()" class="dropdown-item">Sign Out<i class="dropdown-item-icon ti-power-off"></i></a>
-                </div>
-            </li>
-        </ul>
-        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-            <span class="mdi mdi-menu"></span>
-    </div>
-</nav>
-
-<script>
-    function logout() {
-
-        var tokenSession = '<?php echo $_SESSION['token']; ?>';
-        var token = "Bearer" + " " + tokenSession;
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", token);
-        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-        var urlencoded = new URLSearchParams();
-
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: urlencoded,
-            redirect: 'follow'
-        };
-
-        fetch("https://api.klubaderai.com/api/logout", requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                console.log(result)
-            })
-            .catch(error => console.log('error', error));
-    }
-</script>
+        <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+            <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
+                <a class="navbar-brand brand-logo" href="/owner" style="margin-top: -5px; width: 230px; padding: 3px;">
+                    <img src="assets/images/aderaifix.png" alt="logo" /></a>
+                <a class="navbar-brand brand-logo-mini" href="/owner">
+                    <img src="assets/images/logoo.png" alt="logo" />
+                </a>
+            </div>
+            <div class="navbar-menu-wrapper d-flex align-items-center">
+                <!-- <ul class="navbar-nav ml-auto">
+                    <li class="nav-item dropdown d-none d-xl-inline-block user-dropdown">
+                        <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+                            <img class="img-xs rounded-circle" src="assets/images/faces/face8.jpg" alt="Profile image"> </a>
+                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
+                            <div class="dropdown-header text-center">
+                                <img class="img-md rounded-circle" src="assets/images/faces/face8.jpg" alt="Profile image">
+                                <p class="mb-1 mt-3 font-weight-semibold"><?php echo $_SESSION['name'] ?></p>
+                                <p class="font-weight-light text-muted mb-0"><?php echo $_SESSION['email'] ?></p>
+                            </div>
+                            <a href="/profile" class="dropdown-item">My Profile <span class="badge badge-pill badge-danger"></span><i class="dropdown-item-icon ti-dashboard"></i></a>
+                            <a onclick="logout()" class="dropdown-item">Sign Out<i class="dropdown-item-icon ti-power-off"></i></a>
+                        </div>
+                    </li>
+                </ul> -->
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+                    <span class="mdi mdi-menu"></span>
+                </button>
+            </div>
+        </nav>
--- app/includes/config/sidebar.php
+++ app/includes/config/sidebar.php
@@ -1,10 +1,10 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
-        <li class="nav-item nav-profile" style="margin-top: 80px;">
-            <a href="#" class="nav-link">
+        <li class="nav-item nav-profile">
+            <a href=" /profile-admin" class="nav-link">
                 <div class="text-wrapper">
-                    <p class="profile-name">Syahrul Husna</p>
-                    <p class="designation">Premium user</p>
+                    <p class="profile-name">Hallo, <?php echo $_SESSION['name'] ?></p>
+                    <p class="designation"><?php echo $_SESSION['email'] ?></p>
                 </div>
             </a>
         </li>
@@ -15,78 +15,34 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#member" aria-expanded="false" aria-controls="ui-basic">
-                <i class="menu-icon typcn typcn-coffee"></i>
-                <span class="menu-title" style="color: black;"> Member List</span>
-                <i class="menu-arrow"></i>
+            <a class="nav-link" href="/all_member">
+                <i class="menu-icon typcn typcn-document-text"></i>
+                <span class="menu-title" style="color: black;">Member</span>
             </a>
-            <div class="collapse" id="member">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/all_member">Semua Member</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/active_member">Active Member</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/inactive_member">Inactive Member</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/form_member">Tambah Member</a>
-                    </li>
-                </ul>
-            </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#kehadiran" aria-expanded="false" aria-controls="ui-basic">
-                <i class="menu-icon typcn typcn-coffee"></i>
+            <a class="nav-link" href="/kehadiran">
+                <i class="menu-icon typcn typcn-document-text"></i>
                 <span class="menu-title" style="color: black;">Kehadiran</span>
-                <i class=" menu-arrow"></i>
             </a>
-            <div class="collapse" id="kehadiran">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/kehadiran">Semua Kehadiran</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/form_kehadiran">Tambah Kehadiran</a>
-                    </li>
-                </ul>
-            </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#transaksi" aria-expanded="false" aria-controls="ui-basic">
-                <i class="menu-icon typcn typcn-coffee"></i>
-                <span class="menu-title" style="color: black;"> Transaksi</span>
-                <i class=" menu-arrow"></i>
+            <a class="nav-link" href="/transaksi">
+                <i class="menu-icon typcn typcn-document-text"></i>
+                <span class="menu-title" style="color: black;">Transaksi</span>
             </a>
-            <div class="collapse" id="transaksi">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/transaksi">Semua Transaksi</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/form_transaksi">Tambah Transaksi</a>
-                    </li>
-                </ul>
-            </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#paket" aria-expanded="false" aria-controls="ui-basic">
-                <i class="menu-icon typcn typcn-coffee"></i>
-                <span class="menu-title" style="color: black;"> Package List</span>
-                <i class=" menu-arrow"></i>
+            <a class="nav-link" href="/paket">
+                <i class="menu-icon typcn typcn-document-text"></i>
+                <span class="menu-title" style="color: black;">Paket</span>
             </a>
-            <div class="collapse" id="paket">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/paket">Semua Paket</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/form_paket">Tambah Paket</a>
-                    </li>
-                </ul>
-            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/owner">
+                <i class="menu-icon typcn typcn-document-text"></i>
+                <span class="menu-title" style="color: black;">Logout</span>
+            </a>
         </li>
     </ul>
 </nav>
