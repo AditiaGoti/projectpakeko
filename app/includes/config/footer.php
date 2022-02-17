2db8a49ba8e6b6528c9eabc45f731ef7c46b24eb
alsi <syahrulhusna@gmail.com>
2022-02-17 22:15:19 +0700

    table member

--- app/includes/config/footer.php
+++ app/includes/config/footer.php
@@ -3,6 +3,15 @@
   <script type="text/javascript" src="assets/js/shared/off-canvas.js"></script>
   <script type="text/javascript" src="assets/js/shared/misc.js"></script>
   <script type="text/javascript" src="assets/js/demo_1/dashboard.js"></script>
-  <script type="text/javascript" src="assets/js/shared/jquery.dataTables.min.js"></script>
+  <!-- <script type="text/javascript" src="assets/js/shared/jquery.dataTables.min.js"></script>
   <script type="text/javascript" src="assets/js/shared/dataTables.bootstrap.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.dataTables.min.js"></script> -->
+
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
--- app/includes/tables/table_member.php
+++ app/includes/tables/table_member.php
@@ -64,7 +64,7 @@
                             <table class="table table-striped table-bordered table-hover" id="table-data">
                                 <thead>
                                     <tr>
-                                        <th>#</th>
+                                        <th></th>
                                         <th>Nama</th>
                                         <th>Email</th>
                                         <th>No. HP</th>
@@ -81,6 +81,7 @@
                                         var myArray = [];
                                         var tablePaket = document.getElementById("tabel-data");
                                         const url = "https://api.klubaderai.com/api/users";
+
                                         $(document).ready(function() {
                                             $.ajax({
                                                 method: "GET",
@@ -100,7 +101,7 @@
                                                         body += "<td>" + data.alamat + "</td>";
                                                         body += "<td>" + data.expired + "</td>";
                                                         body += "<td>" + data.token + "</td>";
-                                                        body += "<td>" + `<button class="fa fa-pencil" role="button"></button>` + " " + `<button class="fa fa-trash" role="button"></button>` + "</td>";
+                                                        body += "<td>" + `<button class="btn btn-warning" role="button"><i class=" fa fa-pencil"></i></button>` + " " + `<button class="btn btn-danger" role="button"><i class="fa fa-trash"></i></button>` + "</td>";
 
                                                         body += "</tr>";
                                                         $("#table-data tbody").append(body);
@@ -108,6 +109,10 @@
                                                     /*DataTables instantiation.*/
                                                     $("#table-data").DataTable({
                                                         responsive: true,
+                                                        dom: 'Bfrtip',
+                                                        buttons: [
+                                                            'excel', 'pdf', 'print'
+                                                        ]
 
                                                     });
                                                 },
