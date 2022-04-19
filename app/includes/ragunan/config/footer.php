fc6b2d303560b114b54217d5efbfa2536ac791d4
alsi <syahrulhusna@gmail.com>
2022-04-19 15:29:03 +0700

    latest ragunan

---
+++ app/includes/ragunan/config/footer.php
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="../../../../public/assets/vendors/js/vendor.bundle.base.js"></script>
+<script type="text/javascript" src="../../../../public/assets/vendors/js/vendor.bundle.addons.js"></script>
+<script type="text/javascript" src="../../../../public/assets/js/shared/off-canvas.js"></script>
+<script type="text/javascript" src="../../../../public/assets/js/shared/misc.js"></script>
+<script type="text/javascript" src="../../../../public/assets/js/demo_1/dashboard.js"></script>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
---
+++ app/includes/ragunan/config/logout.php
@@ -0,0 +1,30 @@
+<div class="wrap">
+    <div class="loading">
+        <div class="bounceball"></div>
+        <div class="text">NOW LOADING</div>
+    </div>
+</div>
+<script>
+    var tokenSession = '<?php echo $_SESSION['token']; ?>';
+    var token = "Bearer" + " " + tokenSession;
+
+    var settings = {
+        "url": "https://api.tms-klar.com/api/logout",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Authorization": token
+        },
+    };
+
+    $.ajax(settings).done(function(response) {
+        '<?php session_destroy() ?>';
+    });
+
+    function refresh() {
+        window.location.reload();
+    }
+    refresh();
+    // }
+    // });
+</script>
