2385f5219eb870e28ef30ea6518e07a019a11edd
alsi <syahrulhusna@gmail.com>
2022-04-19 13:01:29 +0700

    revisi ragunan tambah route

--- routes/web.php
+++ routes/web.php
@@ -29,76 +29,76 @@ Route::get('/logout', function () {
 
 // Profile
 
-Route::view('/profile-admin', 'admin/view-admin/profile-admin');
-Route::view('/profile-member', 'member/profile-member');
+Route::view('/profile-admin', 'cileungsi/admin/view-admin/profile-admin');
+Route::view('/profile-member', 'cileungsi/member/profile-member');
 
-Route::view('/editprofile-admin', 'admin/view-admin/editprofile-admin');
-Route::view('/editprofile-member', 'member/editprofile-member');
+Route::view('/editprofile-admin', 'cileungsi/admin/view-admin/editprofile-admin');
+Route::view('/editprofile-member', 'cileungsi/member/editprofile-member');
 
-Route::view('/changepass-admin', 'admin/view-admin/changepass-admin');
-Route::view('/changepass-member', 'member/changepass-member');
+Route::view('/changepass-admin', 'cileungsi/admin/view-admin/changepass-admin');
+Route::view('/changepass-member', 'cileungsi/member/changepass-member');
 
 // Admin
 
 Route::get('/admin', function () {
-    return view('admin/view-admin/dashboard');
+    return view('cileungsi/admin/view-admin/dashboard');
 });
 
 Route::get('/transaksi', function () {
-    return view('admin/table-admin/table_transaksi');
+    return view('cileungsi/admin/table-admin/table_transaksi');
 });
 Route::get('/set-transaksi', function () {
-    return view('admin/table-admin/table_set-transaksi');
+    return view('cileungsi/admin/table-admin/table_set-transaksi');
 });
 
 Route::get('/kehadiran', function () {
-    return view('admin/table-admin/table_kehadiran');
+    return view('cileungsi/admin/table-admin/table_kehadiran');
 });
 
 Route::get('/set-kehadiran', function () {
-    return view('admin/table-admin/table_set-kehadiran');
+    return view('cileungsi/admin/table-admin/table_set-kehadiran');
 });
 
 Route::get('/all_member', function () {
-    return view('admin/table-admin/table_allmember');
+    return view('cileungsi/admin/table-admin/table_allmember');
 });
 
 Route::get('/set-all_member', function () {
-    return view('admin/table-admin/table_set-allmember');
+    return view('cileungsi/admin/table-admin/table_set-allmember');
 });
 
 Route::get('/paket', function () {
-    return view('admin/table-admin/table_paket');
+    return view('cileungsi/admin/table-admin/table_paket');
 });
 
 Route::get('/form_kehadiran', function () {
-    return view('admin/form-admin/form_kehadiran');
+    return view('cileungsi/admin/form-admin/form_kehadiran');
 });
 
 Route::get('/form_member', function () {
-    return view('admin/form-admin/form_member');
+    return view('cileungsi/admin/form-admin/form_member');
 });
 
 Route::get('/form_transaksi', function () {
-    return view('admin/form-admin/form_transaksi');
+    return view('cileungsi/admin/form-admin/form_transaksi');
 });
 
 Route::get('/form_paket', function () {
-    return view('admin/form-admin/form_paket');
+    return view('cileungsi/admin/form-admin/form_paket');
 });
 
 Route::get('/formu_member', function () {
-    return view('admin/formu-admin/formu_member');
+    return view('cileungsi/admin/formu-admin/formu_member');
 });
 
 Route::get('/formu_paket', function () {
-    return view('admin/formu-admin/formu_paket');
+    return view('cileungsi/admin/formu-admin/formu_paket');
 });
 
 // Member
 
 Route::get('/member', function () {
-    return view('member/dashboard-member');
+    return view('cileungsi/member/dashboard-member');
 });
 
 //Owner
@@ -106,69 +106,69 @@ Route::get('/member', function () {
 
 
 Route::get('/owner', function () {
-    return view('owner/view-owner/dashboard-owner');
+    return view('cileungsi/owner/view-owner/dashboard-owner');
 });
 
 Route::get('/owalladmin', function () {
-    return view('owner/table-owner/table_alladmin');
+    return view('cileungsi/owner/table-owner/table_alladmin');
 });
 
 Route::get('/owform_admin', function () {
-    return view('owner/form-owner/form_admin');
+    return view('cileungsi/owner/form-owner/form_admin');
 });
 
 Route::get('/owtransaksi', function () {
-    return view('owner/table-owner/table_transaksi');
+    return view('cileungsi/owner/table-owner/table_transaksi');
 });
 
 Route::get('/set-owtransaksi', function () {
-    return view('owner/table-owner/table_set-transaksi');
+    return view('cileungsi/owner/table-owner/table_set-transaksi');
 });
 
 Route::get('/owkehadiran', function () {
-    return view('owner/table-owner/table_kehadiran');
+    return view('cileungsi/owner/table-owner/table_kehadiran');
 });
 Route::get('/set-owkehadiran', function () {
-    return view('owner/table-owner/table_set-kehadiran');
+    return view('cileungsi/owner/table-owner/table_set-kehadiran');
 });
 
 
 Route::get('/owall_member', function () {
-    return view('owner/table-owner/table_allmember');
+    return view('cileungsi/owner/table-owner/table_allmember');
 });
 
 Route::get('/set-owall_member', function () {
-    return view('owner/table-owner/table_set-allmember');
+    return view('cileungsi/owner/table-owner/table_set-allmember');
 });
 
 Route::get('/owpaket', function () {
-    return view('owner/table-owner/table_paket');
+    return view('cileungsi/owner/table-owner/table_paket');
 });
 
 Route::get('/owform_kehadiran', function () {
-    return view('owner/form-owner/form_kehadiran');
+    return view('cileungsi/owner/form-owner/form_kehadiran');
 });
 
 Route::get('/owform_member', function () {
-    return view('owner/form-owner/form_member');
+    return view('cileungsi/owner/form-owner/form_member');
 });
 
 Route::get('/owform_transaksi', function () {
-    return view('owner/form-owner/form_transaksi');
+    return view('cileungsi/owner/form-owner/form_transaksi');
 });
 
 Route::get('/owform_paket', function () {
-    return view('owner/form-owner/form_paket');
+    return view('cileungsi/owner/form-owner/form_paket');
 });
 
 Route::get('/owformu_admin', function () {
-    return view('owner/formu-owner/formu_admin');
+    return view('cileungsi/owner/formu-owner/formu_admin');
 });
 
 Route::get('/owformu_member', function () {
-    return view('owner/formu-owner/formu_member');
+    return view('cileungsi/owner/formu-owner/formu_member');
 });
 
 Route::get('/owformu_paket', function () {
-    return view('owner/formu-owner/formu_paket');
+    return view('cileungsi/owner/formu-owner/formu_paket');
 });
