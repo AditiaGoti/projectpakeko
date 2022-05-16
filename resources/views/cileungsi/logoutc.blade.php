032912a7f318ec9f353a1708e5de1e9ba6fdfecd
alsi <syahrulhusna@gmail.com>
2022-05-16 21:43:37 +0700

    add logoutc

---
+++ resources/views/cileungsi/logoutc.blade.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['login_status'])) {
+    header("location: /Ragunan");
+    exit;
+}
+?>
+<?php include(app_path() . '/includes/ragunan/config/header.php'); ?>
+<div class="wrap">
+    <div class="loading">
+        <div class="bounceball"></div>
+        <div class="text">NOW LOADING</div>
+    </div>
+</div>
+<script>
+    var tokenSession = '<?php echo $_SESSION['token']; ?>';
+    var token = "Bearer" + " " + tokenSession;
+
+    var settings = {
+        "url": "https://ragunan.tms-klar.com/api/logout",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Authorization": token
+        },
+    };
+
+    $.ajax(settings).done(function(response) {
+        '<?php session_destroy() ?>';
+    });
+
+    function refresh() {
+        window.location.reload();
+    }
+    refresh();
+    // }
+    // });
+</script>
